44fb619a8f1f8c4b1d88da83c25cae8fcffc36bf
ankitkumargupta <ankitgupta6881@gmail.com>
2025-01-11 22:20:20 +0530

    learning form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import { useRef } from 'react'
+import React from "react";
+import { useRef } from "react";
 
 function App() {
-
   const nameRef = useRef(null);
   const ageRef = useRef(null);
 
@@ -15,24 +14,40 @@ function App() {
     console.log("Name: ", name);
     console.log("Age: ", age);
 
-    nameRef.current.value = '';
-    ageRef.current.value = '';
-
-  }
-
+    nameRef.current.value = "";
+    ageRef.current.value = "";
+  };
 
   return (
-    <div className='w-full h-screen flex justify-center items-center'>
-      <form className='w-60 h-52 items-center p-4 border border-zinc-400' action="" onSubmit={handleSubmit}>
-        <input className='m-2 border border-zinc-400' type="text" placeholder='Name' ref={nameRef} />
-        <input className='m-2 border border-zinc-400' type="text" placeholder='Age' ref={ageRef} />
-        <div>
-          <input type="submit" className='m-2 px-3 py-2 rounded-md text-sm bg-zinc-400'/>
-        </div>
-      </form>
-    </div>
-    
-  )
+    <>
+      <div className="w-full h-screen flex justify-center items-center">
+        <form
+          className="w-60 h-52 items-center p-4 border border-zinc-400"
+          action=""
+          onSubmit={handleSubmit}
+        >
+          <input
+            className="m-2 border border-zinc-400"
+            type="text"
+            placeholder="Name"
+            ref={nameRef}
+          />
+          <input
+            className="m-2 border border-zinc-400"
+            type="text"
+            placeholder="Age"
+            ref={ageRef}
+          />
+          <div>
+            <input
+              type="submit"
+              className="m-2 px-3 py-2 rounded-md text-sm bg-zinc-400"
+            />
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
